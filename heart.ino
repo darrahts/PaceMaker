7c580cc860cf45d6dd1dbe659b2d368c42db5a32
Timothy Darrah <31658834+darrahts@users.noreply.github.com>
2018-04-16 13:46:03 -0500

    Add files via upload

---
+++ heart.ino
@@ -0,0 +1,182 @@
+
+
+#define BTN 52
+
+#define PACE_SIGNAL_PIN 2
+
+
+
+double ecgVals[] = {.025, .02, .022, .018, .018, .02, .01, .1, .19, .1, -.05, -.07, -.14, .6, 1.2, .6, -.2, -.04, .01, .02, .04, .03, .08, .15, .19, .2, .21, .2, .19, .15, .08, .06, .05, .04, .03, .025};
+const int len = sizeof(ecgVals)/sizeof(double);
+double newEcgVals[len];
+
+double ecgValsSlow[] = {.025, .02, .022, .018, .018, .02, .01, .1, .19, .16, .14, .12, .1, 0, -.01, -.02, -.03, -.04, -.04, -.04, -.05, -.06, -.07, -.07, -.08, -.14, .6, 1.2, .6, -.2, -.04, .01, .02, .04, .03, .08, .15, .19, .2, .21, .2, .19, .15, .08, .06, .05, .04, .03, .025};
+const int len2 = sizeof(ecgValsSlow)/sizeof(double);
+double newEcgValsSlow[len2];
+
+int mode = 1;
+int BTNState = 1;
+volatile int j = 0;
+
+int value = 0;
+
+volatile bool paced = false;
+//volatile bool ignore = false;
+
+//volatile int k = 0;
+
+long t1 = millis();
+
+void Pace()
+{
+    if(j > 11 && j < 17)
+    {
+       // k = j;
+        paced = true;
+        j = 24;
+        return;
+    }
+    else
+    {
+        //ignore = true;
+    }
+    return;
+}
+
+//*********************************************************************** MAP
+
+int MapF(double x, double inMin, double inMax, double outMin, double outMax)
+{
+    return int((x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
+}
+
+//***********************************************************************
+//*********************************************************************** SETUP
+//***********************************************************************
+void setup() 
+{
+    SerialUSB.begin(9600);
+    //Serial.begin(9600);
+    while(!SerialUSB) ;
+    for(int i=0; i < len; i++)
+    {
+        newEcgVals[i] = MapF(ecgVals[i], -.2, 1.2, 50, 4095);
+        SerialUSB.print(newEcgVals[i]);
+        SerialUSB.print(", ");
+    }
+    SerialUSB.println();
+
+    for(int i=0; i < len2; i++)
+    {
+        newEcgValsSlow[i] = MapF(ecgValsSlow[i], -.2, 1.2, 50, 4095);
+        SerialUSB.print(newEcgValsSlow[i]);
+        SerialUSB.print(", ");
+    }
+    SerialUSB.println();
+    SerialUSB.println(mode);
+
+    analogWriteResolution(12);
+    pinMode(DAC1, OUTPUT);
+    pinMode(BTN, INPUT_PULLUP);
+    analogWrite(DAC1, 0);
+    delay(3000);
+
+    attachInterrupt(digitalPinToInterrupt(PACE_SIGNAL_PIN), Pace, RISING);
+    delay(5000);
+
+}
+
+void loop() 
+{
+//    if(paced)
+//    {
+//        SerialUSB.print("PACED at: ");
+//        SerialUSB.println(k);
+//        paced = false;
+//    }
+//    if(ignore)
+//    {
+//        SerialUSB.print("IGNORED at: ");
+//        SerialUSB.println(k);
+//        ignore = false;
+//    }
+    BTNState = digitalRead(BTN);
+    if(mode != BTNState)
+    {
+        mode = BTNState;
+        SerialUSB.print("mode: ");
+        SerialUSB.println(mode);
+    }
+    if(BTNState == 1)
+    {
+        while(j < len)
+        {
+            analogWrite(DAC1,newEcgVals[j]);
+            j++;
+            delay(26);
+            //SerialUSB.println(newEcgVals[j++]);
+        }
+        j = 0;
+    }
+    else
+    {
+        while(j < len2)
+        {
+            analogWrite(DAC1,newEcgValsSlow[j]);
+            j++;
+            delay(26);
+            //SerialUSB.println(newEcgValsSlow[j++]);
+        }
+        j = 0;
+    }
+}
+
+
+void Test()
+
+{
+    analogWrite(DAC1, value );  
+    value++;
+    delay(5);
+    if(value >= 4095)
+    {
+        value = 0;
+        delay(5000); 
+    }
+}
+
+
+void Test2()
+{
+    analogWrite(DAC1, 0);
+    delay(25);
+    int k = 0;
+    for(int m = 0; m < 1; m++)
+    {
+        while(k < len)
+        {
+            analogWrite(DAC1,newEcgVals[k]);
+            k++;
+            delay(26);
+            //Serial.println(newEcgVals[k++]);
+            //SerialUSB.println(newEcgVals[k++]);
+        }
+        k = 0;
+    }
+    analogWrite(DAC1, 0);
+    delay(10000);
+    //Serial.println();
+    //SerialUSB.println("waiting");
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ paceMaker.ino
@@ -0,0 +1,353 @@
+#include <stdio.h>
+
+#define PACE_SIGNAL_PIN 2
+#define ECG_IN_PIN A1
+
+
+enum State
+{
+    A = 0, // initial state
+    P, //
+    Q, //
+    R, //
+    Rp, // peak
+    S, //
+    T  //
+};
+
+
+//******************************************************* VARIABLES
+
+bool timerReady = true;
+
+long t1 = millis();
+long t2 = millis();
+long initTime = millis();
+
+long errorTime = millis();
+long lastPace = millis();
+
+long beatTime = millis();
+
+int reading = 0;
+int val = 0;
+enum State state = A;
+
+long duration = 0;
+
+bool triggered = false;
+//bool initialized = false;
+
+int beat = 0;
+int bpm = 0;
+
+int pC = 0;
+int qC = 0;
+int rC = 0; 
+int rpC = 0;
+int sC = 0;
+int tC = 0;
+
+int timesp[] = {0,0,0,0,0,0,0,0,0,0};
+int timesq[] = {0,0,0,0,0,0,0,0,0,0};
+int timesr[] = {0,0,0,0,0,0,0,0,0,0};
+int timesrp[] = {0,0,0,0,0,0,0,0,0,0};
+int timess[] = {0,0,0,0,0,0,0,0,0,0};
+int timest[] = {0,0,0,0,0,0,0,0,0,0};
+
+int p = 0;
+int q = 0;
+int r = 0;
+int rp = 0;
+int s = 0;
+int t = 0;
+
+
+//******************************************************* PACE
+void Pace()
+{
+    if(millis() - lastPace > 800)
+    {
+        digitalWrite(PACE_SIGNAL_PIN, HIGH);
+        //Serial.print("PACED at: ");
+        //Serial.println(reading);
+        errorTime = millis();
+        while(reading > 160 || reading < 120)
+        {
+            Poll();
+        }
+        //Serial.println(millis() - errorTime);
+        //Serial.println();
+
+        digitalWrite(PACE_SIGNAL_PIN, LOW);
+        lastPace = millis();
+    }
+    return;
+}
+
+//******************************************************* POLL
+void Poll()
+{
+    reading = int(analogRead(ECG_IN_PIN));
+    return;
+}
+
+//******************************************************* UPDATE STATE VARIABLES
+
+void UpdateStateVariables(int &arrayCounter,int &stateCounter, int timesArray[])
+{
+    //Serial.print("state: ");
+    //Serial.print(int(state));
+    //Serial.print("     count: ");
+    //Serial.println(stateCounter);
+    timesArray[arrayCounter] = stateCounter;
+    stateCounter = 0;
+    arrayCounter++;
+    //Serial.print("arrayCounter: ");
+    //Serial.println(arrayCounter);
+    if(arrayCounter == 10)
+    {
+//        Serial.print("state: ");
+//        Serial.print(int(state));
+//        Serial.print("     count: ");
+//        for(int j = 0; j < 10; j++)
+//        {
+//            Serial.print(timesArray[j]);
+//            Serial.print(", ");
+//        }
+//        Serial.println();
+        arrayCounter = 0;
+    }
+}
+
+
+//******************************************************* SetState
+void SetState(int nextState)
+{
+//    if(state == P)
+//    {
+//        Serial.println("******");
+//    }
+//    Serial.print("state: ");
+//    Serial.print(int(state));
+    
+    state = State(nextState);
+    
+    if(!timerReady)
+    {
+        t2 = millis();
+        duration = (t2-t1);
+        timerReady = true;
+//        Serial.print("     duration: ");
+//        Serial.print(duration);
+//        if(state == Rp || state == S)
+//        {
+//            Serial.print(" ");
+//        }
+//        Serial.print("     next state: ");
+//        Serial.print(int(state));
+    }
+    if(timerReady)
+    {
+        t1 = millis();
+        timerReady = false;
+    }
+//   Serial.println();
+    return;
+}
+
+//******************************************************* STEP
+void Step()
+{
+    
+    if(state == A)
+    {
+        //Serial.println("A");
+        if(reading > 150)
+        {
+            SetState(state+1);
+        }
+        return;
+    }
+    
+    if(state == P)
+    {
+        //Serial.println("P");
+        pC++;
+        if(reading > 200 && reading < 225)
+        {
+            UpdateStateVariables(p, pC, timesp);
+            SetState(state+1);
+        }
+        return;
+    }
+
+    
+    if(state == Q)
+    {
+        //Serial.println("Q");
+        qC++;
+        if(reading < 140 && reading > 130)
+        {
+            UpdateStateVariables(q, qC, timesq);
+            SetState(state+1);
+            return;
+        }
+//        if(!triggered && !timerReady && millis() - t1 > 150)
+//        {
+//            //Pace();
+//            triggered = true;
+//        }
+        else if(!triggered && qC > 29)
+        {
+            Pace();
+            triggered = true;
+            UpdateStateVariables(q, qC, timesq);
+            SetState(state+1);
+        }
+        return;
+    }
+    
+    if(state == R)
+    {
+        if(triggered)
+        {
+            triggered = false;
+        }
+        //Serial.println("R");
+        rC++;
+        if(reading > 550)
+        {
+            UpdateStateVariables(r, rC, timesr);
+            SetState(state+1);
+            beat++;
+            if(beat == 12)
+            {
+                bpm = int(60 / double((millis() - beatTime) / beat) * 1000);
+                Serial.print("BPM: ");
+                Serial.println(bpm);
+                beat = 0;
+                beatTime = millis();
+                
+            }
+        }
+        return;
+    }
+
+    if(state == Rp)
+    {
+        //Serial.println("Rp");
+        rpC++;
+        if(reading < 150)
+        {
+            UpdateStateVariables(rp, rpC, timesrp);
+            SetState(state+1);
+        }
+        return;
+    }
+    
+    if(state == S)
+    {
+        //Serial.println("S");
+        sC++;
+        if(reading > 195)
+        {
+            UpdateStateVariables(s, sC, timess);
+            SetState(state+1);
+        }
+        return;
+    }
+    
+    if(state == T)
+    {
+        //Serial.println("T");
+        tC++;
+        if(reading < 195 && reading > 188)
+        {
+            UpdateStateVariables(t, tC, timest);
+            SetState(1);
+        }
+
+        return;
+    }
+}
+
+//*******************************************************
+//******************************************************* SETUP
+//*******************************************************
+void setup() 
+{
+    pinMode(ECG_IN_PIN, INPUT);
+    pinMode(PACE_SIGNAL_PIN, OUTPUT);
+    digitalWrite(PACE_SIGNAL_PIN, LOW);
+    Serial.begin(9600);
+    delay(3000);
+    Serial.println("initialized.");
+    beatTime = millis();
+}
+
+//*******************************************************
+//******************************************************* MAIN
+//*******************************************************
+void loop() 
+{
+     Poll();
+     //PrintVal();
+     Step();
+     delay(5);
+     //Serial.println(reading);
+}
+
+
+void PrintVal()
+{
+    Serial.print(reading);
+    Serial.print(", ");
+}
+
+
+//******************************************************* TEST
+void Test()
+{
+   while(true)
+    {
+        delay(8);
+        int reading = int(analogRead(A1));
+        if(reading > 115 && val != reading)
+        {
+            if(!timerReady)
+            {
+                t1 = millis();
+                timerReady = true;
+            }
+            Serial.print(analogRead(A1));
+            Serial.print(",");
+            val = reading; 
+        }
+        if(reading < 115)
+        {
+          //Serial.print("reading is: ");
+          //Serial.println(reading);
+            if(timerReady)
+            {
+                Serial.println();
+                Serial.println();
+                t2 = millis();
+                Serial.println(t2-t1);
+                timerReady = false;
+                Serial.println();
+                Serial.println();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
